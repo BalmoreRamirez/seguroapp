11a8261bb643e4162cb85f171d960d9d0574d0d3
Balmore Ramirez <jbramirez@innovacion.gob.sv>
2024-05-27 15:51:06 -0600

    buscador buscador

--- src/AuthUsuario/auth_usuario.module.ts
+++ src/AuthUsuario/auth_usuario.module.ts
@@ -15,7 +15,7 @@ import { jwtConstants } from './constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '10m' },
+      signOptions: { expiresIn: '2m' },
     }),
   ],
   controllers: [AuthUsuarioController],
